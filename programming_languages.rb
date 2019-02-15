419238a4fed876ec6e1ec5179c2e33d5ef7f5f49
Louie O'Grady <l.ogrady@gmail.com>
2019-02-15 21:24:31 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  nhash = {}
+  languages.each do |lang_style, lname|
+    lname.each do |lang_type|
+      nhash[lname] = lang_type
+      if lang_style == languages[:oo]
+        nhash[:style] = [:oo]
+      else
+        nhash[:style] << lang_style
+      end
+    end
+  end 
+nhash
 end
+
+#nhash = {}
+#languages.each do |hash|
+ # binding pry
+#hash.merge!(nhash)
